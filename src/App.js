22bc7f302715f5be370578b3d6c7bd914b4d3332
Mohammad Maleki <m.maleki@mahsan.co>
2025-02-03 10:31:58 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App({ callback }) {
+  const notify = () => {
+    console.log("service worker update waiting");
+    toast("A new version is available: exit the app to update");
+  };
+
+  React.useEffect(() => {
+    callback.onUpdate = () => {
+      notify();
+    };
+  }, [callback]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>This is react app V1</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
