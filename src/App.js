98a983d75fb889ad7dc02acea568786847551cd5
Mohammad Maleki <m.maleki@mahsan.co>
2025-04-07 09:55:21 +0330

    update

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App({ callback }) {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is react app <strong>V.14</strong>
+          This is react app <strong>V.15</strong>
         </p>
         <a
           className="App-link"
